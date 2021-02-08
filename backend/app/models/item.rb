14cf7a87c2b906b87286af7ceb6e593a7000c460
ruiinoue <jingshangrui422@gmail.com>
2021-02-08 15:33:38 +0900

    モデルのテスト実行

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :todo
+
+  validates :name, presence: true
+  validates :done, presence: true
 end
--- backend/spec/models/todo_spec.rb
+++ backend/spec/models/todo_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "リレーションの確認" do
+    describe "1対多の関係" do
+      it "itemsとhave_manyでdestroyアクションで依存関係である" do
+        should have_many(:items).dependent(:destroy)
+      end
+    end
+  end
+
+  describe "有効性を確認" do
+    let(:todo) { Todo.new(params) }
+    let(:params) { { title: 'Mozart', created_by: "1" } }
+
+    context "無効パターン" do
+      it "titleが無効" do
+        todo.title = " "
+        expect(todo).to be_invalid
+      end
+
+      it "created_byが無効" do
+        todo.created_by = " "
+        expect(todo).to be_invalid
+      end
+
+      it "titleとcreated_by共に無効" do
+        todo.title = " "
+        todo.created_by = " "
+        expect(todo).to be_invalid
+      end
+    end
+
+    context "有効パターン" do
+      it "titleとcreated_by共に有効" do
+        expect(todo).to be_valid
+      end
+    end
+  end
+
+  describe "存在性を確認" do
+    context "Todoの存在を確認" do
+      it "titleが存在する" do
+        should validate_presence_of(:title)
+      end
+
+      it "created_byが存在する" do
+        should validate_presence_of(:created_by)
+      end
+    end
+  end
 end
