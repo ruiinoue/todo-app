397060f9d681fdec40ec1bf2422665a38e2f5881
ruiinoue <jingshangrui422@gmail.com>
2021-02-15 22:12:22 +0900

    Rails APIの動作確認

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -23,8 +23,18 @@
         >
           GitHub
         </a>
-        <button @click="getTodos">確認</button>
         {{ todos }}
+        <button @click="getTodos">確認</button>
+        <button @click="postTodos">追加</button>
+        <button @click="putTodos">更新</button>
+        <button @click="showTodos">表示</button>
+        <button @click="daleteTodos">削除</button>
+        {{ items }}
+        <button @click="getItems">確認</button>
+        <button @click="postItems">追加</button>
+        <button @click="putItems">更新</button>
+        <button @click="showItems">表示</button>
+        <button @click="deleteItems">削除</button>
       </div>
     </div>
   </div>
@@ -40,13 +50,50 @@ export default {
   data() {
     return {
       todos: [],
+      items: [],
     }
   },
   methods: {
     async getTodos() {
       const res = await this.$axios.$get('http://localhost:5000/todos')
       this.todos = res
-    }
+    },
+    async postTodos() {
+      const res = await this.$axios.$post('http://localhost:5000/todos', { title: "Create", created_by: 1 })
+      this.todos.push(res)
+    },
+    async showTodos() {
+      const res = await this.$axios.$get(`http://localhost:5000/todos/1`)
+      this.todos = res
+    },
+    async putTodos() {
+      const res = await this.$axios.$put(`http://localhost:5000/todos/1`, { title: "Change" })
+      this.todos = res
+    },
+    async daleteTodos() {
+      const res = await this.$axios.$delete(`http://localhost:5000/todos/2`)
+      this.todos = res
+    },
+    async getItems() {
+      const res = await this.$axios.$get('http://localhost:5000/todos/1/items')
+      this.items = res
+    },
+    async postItems() {
+      const res = await this.$axios.$post('http://localhost:5000/todos/1/items', { name: 'Create', done: false })
+      this.items.push(res)
+    },
+    async showItems() {
+      const res = await this.$axios.$get(`http://localhost:5000/todos/1/items/1`)
+      this.todos = res
+    },
+    async putItems() {
+      const res = await this.$axios.$put('http://localhost:5000/todos/1/items/1', { name: 'Change', done: true })
+      this.items = res
+    },
+    async deleteItems() {
+      const res = await this.$axios.$delete('http://localhost:5000/todos/1/items/2')
+      this.items = res
+    },
   }
 }
 </script>
