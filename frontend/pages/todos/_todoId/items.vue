3251c97076d606ea5b87350b25ff88349e23e1b6
ruiinoue <jingshangrui422@gmail.com>
2021-02-21 21:29:28 +0900

    itemsのCRUDの実装

---
+++ frontend/pages/todos/_todoId/items.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>itemの一覧</div>
+    <div>itemを格納しているtodoのID: {{ todoId }}</div>
+    <div v-if="items.length === 0">itemなし</div>
+    <ul v-for="(item, index) in items" :key="`items-${index}`">
+      <li>{{ item }}</li>
+    </ul>
+    <nuxt-link :to="{ name: 'todos-todoid', params: { todoid: todoId } }">todoへ戻る</nuxt-link>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+      todoId: this.$route.params.todoId
+    }
+  },
+  async asyncData({ route, store }) {
+    await store.dispatch('fetchItems', route.params.todoId)
+  },
+  mounted() {},
+  computed: {
+    ...mapGetters(['items'])
+  },
+}
+</script>
+
+<style scoped>
+</style>
